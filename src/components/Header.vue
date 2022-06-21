b3b99ce4c41dc177cf88a0fba7905eb31eaf99e8
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2022-06-21 17:55:16 +0530

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <header>
+    <h1>Navbar</h1>
+    <router-link to="/">Home </router-link>
+    <router-link to="/about"> About </router-link>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-evenly;
+  border: 2px solid black;
+  align-items: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
