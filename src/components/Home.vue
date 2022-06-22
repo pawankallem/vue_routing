29f9ae60a7847287b29fe695288c544d7a1c29b2
Pavan Kallem <90694105+pawankallem@users.noreply.github.com>
2022-06-22 11:37:56 +0530

    Routing working and api call working in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,64 @@
 
 <template>
   <div>
-    <h1>This is Home page</h1>
+    <h1>Home page</h1>
+    <h1 v-show="isLoading">Loading . . . .</h1>
+    <table v-show="!isLoading">
+      <thead>
+        <tr>
+          <th>S.No</th>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(x, i) in apiData" v-bind:key="i">
+          <td>{{ x.id }}</td>
+          <td>{{ x.title }}</td>
+          <td>{{ x.price }}</td>
+          <td>{{ x.description }}</td>
+          <td>{{ x.category }}</td>
+          <td><img v-bind:src="x.image" alt="" /></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      apiData: [],
+      isLoading: true,
+    };
+  },
+  beforeMount() {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        this.apiData = res.data;
+        this.isLoading = false;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style scoped>
+table,
+th,
+td {
+  border: 1px solid;
+}
+img {
+  width: 50px;
+}
 </style>
